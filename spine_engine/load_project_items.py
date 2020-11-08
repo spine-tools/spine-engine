24cf54816dba51e55d9ab2bbbd2e5eb1dbb92ed9
Manuel Marin <manuelma@kth.se>
2020-11-08 09:23:06 +0100

    Avoid race conditions in parsing spine_items
    This may happen when running several engines at a time.
    I wonder if there's a better way of parsing project items...
    This seems to work though.

--- spine_engine/load_project_items.py
+++ spine_engine/load_project_items.py
@@ -18,12 +18,15 @@ Functions to load project item modules.
 import pathlib
 import importlib
 import importlib.util
+from multiprocessing import Lock
 from .utils.helpers import Singleton
 
 
 class ProjectItemLoader(metaclass=Singleton):
-    _specification_factories = {}
-    _executable_item_classes = {}
+    _specification_factories = []
+    _executable_item_classes = []
+    _specification_factories_lock = Lock()
+    _executable_item_classes_lock = Lock()
 
     def load_item_specification_factories(self):
         """
@@ -32,24 +35,26 @@ class ProjectItemLoader(metaclass=Singleton):
         Returns:
             dict: a map from item type to specification factory
         """
-        if self not in self._specification_factories:
-            import spine_items
+        with self._specification_factories_lock:
+            if not self._specification_factories:
+                import spine_items
 
-            items_root = pathlib.Path(spine_items.__file__).parent
-            self._specification_factories[self] = factories = dict()
-            for child in items_root.iterdir():
-                if child.is_dir() and child.joinpath("specification_factory_qt_free.py").exists():
-                    spec = importlib.util.find_spec(f"spine_items.{child.stem}.specification_factory_qt_free")
-                elif child.is_dir() and child.joinpath("specification_factory.py").exists():
-                    spec = importlib.util.find_spec(f"spine_items.{child.stem}.specification_factory")
-                else:
-                    continue
-                m = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(m)
-                if hasattr(m, "SpecificationFactory"):
-                    item_type = m.SpecificationFactory.item_type()
-                    factories[item_type] = m.SpecificationFactory
-        return self._specification_factories[self]
+                items_root = pathlib.Path(spine_items.__file__).parent
+                factories = dict()
+                for child in items_root.iterdir():
+                    if child.is_dir() and child.joinpath("specification_factory_qt_free.py").exists():
+                        spec = importlib.util.find_spec(f"spine_items.{child.stem}.specification_factory_qt_free")
+                    elif child.is_dir() and child.joinpath("specification_factory.py").exists():
+                        spec = importlib.util.find_spec(f"spine_items.{child.stem}.specification_factory")
+                    else:
+                        continue
+                    m = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(m)
+                    if hasattr(m, "SpecificationFactory"):
+                        item_type = m.SpecificationFactory.item_type()
+                        factories[item_type] = m.SpecificationFactory
+                self._specification_factories.append(factories)
+        return self._specification_factories[0]
 
     def load_executable_item_classes(self):
         """
@@ -58,22 +63,24 @@ class ProjectItemLoader(metaclass=Singleton):
         Returns:
             dict: a map from item type to the executable item class
         """
-        if self not in self._executable_item_classes:
-            import spine_items
+        with self._executable_item_classes_lock:
+            if not self._executable_item_classes:
+                import spine_items
 
-            items_root = pathlib.Path(spine_items.__file__).parent
-            self._executable_item_classes[self] = executable_items = dict()
-            for child in items_root.iterdir():
-                if child.is_dir() and child.joinpath("executable_item_qt_free.py").exists():
-                    spec = importlib.util.find_spec(f"spine_items.{child.stem}.executable_item_qt_free")
-                elif child.is_dir() and child.joinpath("executable_item.py").exists():
-                    spec = importlib.util.find_spec(f"spine_items.{child.stem}.executable_item")
-                else:
-                    continue
-                m = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(m)
-                if hasattr(m, "ExecutableItem"):
-                    item_class = m.ExecutableItem
-                    item_type = item_class.item_type()
-                    executable_items[item_type] = item_class
-        return self._executable_item_classes[self]
+                items_root = pathlib.Path(spine_items.__file__).parent
+                executable_items = dict()
+                for child in items_root.iterdir():
+                    if child.is_dir() and child.joinpath("executable_item_qt_free.py").exists():
+                        spec = importlib.util.find_spec(f"spine_items.{child.stem}.executable_item_qt_free")
+                    elif child.is_dir() and child.joinpath("executable_item.py").exists():
+                        spec = importlib.util.find_spec(f"spine_items.{child.stem}.executable_item")
+                    else:
+                        continue
+                    m = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(m)
+                    if hasattr(m, "ExecutableItem"):
+                        item_class = m.ExecutableItem
+                        item_type = item_class.item_type()
+                        executable_items[item_type] = item_class
+                self._executable_item_classes.append(executable_items)
+        return self._executable_item_classes[0]
