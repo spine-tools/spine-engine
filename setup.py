fd94149ea93eb2af7dfef29ce1cc317884849f3d
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2021-04-30 18:18:49 +0300

    Freeze required package versions for release

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ with open("spine_engine/version.py") as fp:
     exec(fp.read(), version)
 
 install_requires = [
-    "dagster >= 0.9.15, <= 0.10.1",
-    "sqlalchemy >= 1.3",
-    "numpy >= 1.15.1",
-    "datapackage >= 1.15",
+    "dagster==0.10.1",
+    "sqlalchemy==1.3.24",
+    "numpy==1.20.2",
+    "datapackage==1.15.2",
     "spinedb_api >= {}".format(REQUIRED_SPINEDB_API_VERSION),
 ]
 
