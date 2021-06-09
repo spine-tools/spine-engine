f7a393f1021ffcf6d20edf872551451136893633
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2021-06-09 11:17:48 +0300

    Relax required package version restrictions
    Remove pyrsisten version requirement because newer version of tableschema should take that into account

--- setup.py
+++ setup.py
@@ -25,7 +25,13 @@ version = {}
 with open("spine_engine/version.py") as fp:
     exec(fp.read(), version)
 
-install_requires = ["dagster==0.10.1", "sqlalchemy==1.3.24", "numpy==1.20.2", "datapackage==1.15.2", "spinedb_api"]
+install_requires = [
+    "dagster>=0.10.1",
+    "sqlalchemy>=1.3.24",
+    "numpy>=1.20.2",
+    "datapackage>=1.15.2",
+    "spinedb_api"
+]
 
 
 setup(
