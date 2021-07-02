135435d7d434629a69fc01c40984969eb085d10f
PekkaSavolainen <43600314+PekkaSavolainen@users.noreply.github.com>
2021-07-02 19:02:06 +0300

    Issue 1047 activate conda envs (#38)
    Support for activating Conda environments
    
    re Spine-project/Spine-Toolbox#1047

--- spine_engine/execution_managers/kernel_execution_manager.py
+++ spine_engine/execution_managers/kernel_execution_manager.py
@@ -20,8 +20,10 @@ import os
 import sys
 import subprocess
 from jupyter_client.manager import KernelManager
+from jupyter_client.kernelspec import NoSuchKernel
 from ..utils.helpers import Singleton
 from .execution_manager_base import ExecutionManagerBase
+from spine_engine.execution_managers.conda_kernel_spec_manager import CondaKernelSpecManager
 
 
 class _KernelManagerFactory(metaclass=Singleton):
@@ -48,7 +50,7 @@ class _KernelManagerFactory(metaclass=Singleton):
             self._kernel_managers[key] = KernelManager(kernel_name=kernel_name)
         return self._kernel_managers[key]
 
-    def new_kernel_manager(self, kernel_name, group_id, logger, extra_switches=None, **kwargs):
+    def new_kernel_manager(self, kernel_name, group_id, logger, extra_switches=None, environment="", **kwargs):
         """Creates a new kernel manager for given kernel and group id if none exists.
         Starts the kernel if not started, and returns it.
 
@@ -58,15 +60,24 @@ class _KernelManagerFactory(metaclass=Singleton):
             logger (LoggerInterface): for logging
             extra_switches (list, optional): List of additional switches to julia or python.
                 These come before the 'programfile'.
+            environment (str): "conda" to launch a Conda kernel spec. "" for a regular kernel spec
             `**kwargs`: optional. Keyword arguments passed to ``KernelManager.start_kernel()``
 
         Returns:
             KernelManager
         """
         km = self._make_kernel_manager(kernel_name, group_id)
+        conda_exe = kwargs.pop("conda_exe", "")
+        if environment == "conda":
+            km.kernel_spec_manager = CondaKernelSpecManager(conda_exe=conda_exe)
         msg_head = dict(kernel_name=kernel_name)
         if not km.is_alive():
-            if not km.kernel_spec:
+            try:
+                if not km.kernel_spec:  # TODO: Check if this is needed in addition to catching NoSuchKernel?
+                    msg = dict(type="kernel_spec_not_found", **msg_head)
+                    logger.msg_kernel_execution.emit(msg)
+                    raise RuntimeError
+            except NoSuchKernel:
                 msg = dict(type="kernel_spec_not_found", **msg_head)
                 logger.msg_kernel_execution.emit(msg)
                 raise RuntimeError
@@ -133,6 +144,7 @@ class KernelExecutionManager(ExecutionManagerBase):
         workdir=None,
         startup_timeout=60,
         extra_switches=None,
+        environment="",
         **kwargs,
     ):
         """
@@ -145,6 +157,7 @@ class KernelExecutionManager(ExecutionManagerBase):
             startup_timeout (int, optional): How much to wait for the kernel, used in ``KernelClient.wait_for_ready()``
             extra_switches (list, optional): List of additional switches to launch julia.
                 These come before the 'programfile'.
+            environment (str): "conda" to launch a Conda kernel spec. "" for a regular kernel spec.
             **kwargs (optional): Keyword arguments passed to ``KernelManager.start_kernel()``
         """
         super().__init__(logger)
@@ -157,7 +170,7 @@ class KernelExecutionManager(ExecutionManagerBase):
         # Don't show console when frozen
         kwargs["creationflags"] = subprocess.CREATE_NO_WINDOW if sys.platform == "win32" else 0
         self._kernel_manager = _kernel_manager_factory.new_kernel_manager(
-            kernel_name, group_id, logger, cwd=self._workdir, extra_switches=extra_switches, **kwargs
+            kernel_name, group_id, logger, cwd=self._workdir, extra_switches=extra_switches, environment=environment, **kwargs
         )
         self._kernel_client = self._kernel_manager.client() if self._kernel_manager is not None else None
         self._startup_timeout = startup_timeout
