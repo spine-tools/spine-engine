0c3e0c0408dee37cc4921729548dac3e021af71c
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2021-09-03 11:57:05 +0300

    Clean up version string

--- spine_engine/version.py
+++ spine_engine/version.py
@@ -8,4 +8,4 @@
 # Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################################################
-__version__ = "0.11.0-final.0"
+__version__ = "0.11.0"
