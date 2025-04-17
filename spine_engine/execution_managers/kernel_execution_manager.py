152af793da61c52179eac8c61990cf1060de7654
ptsavol <43600314+ptsavol@users.noreply.github.com>
2025-04-17 14:11:40 +0300

    Fix bug in kernel execution (#162)
    - The bug mistakenly gave a green check mark to a Tool on Design View after execution even when Jupyter Console fails to start. For example, when kernel does not exist.

--- spine_engine/execution_managers/kernel_execution_manager.py
+++ spine_engine/execution_managers/kernel_execution_manager.py
@@ -299,7 +299,7 @@ class KernelExecutionManager(ExecutionManagerBase):
 
     def run_until_complete(self):
         if self._kernel_client is None:
-            return 0
+            return -1
         self._kernel_client.start_channels()
         run_succeeded = self._do_run()
         self._kernel_client.stop_channels()
